f74d2cb0eb0c923d2a5e0e1ec67f211337047a2f
Remy Gavard <r.gavard@gmail.com>
2020-01-17 16:54:35 +0000

    Initial commit

---
+++ R/get_inventors_df.R
@@ -0,0 +1,22 @@
+#' Extract inventors information as a tibble
+#' 
+#' 
+#' @param pv_res Takes results from get_patents_data 
+#' @return A tibble with the inventors information
+#' 
+#' @importFrom magrittr %>%
+#' @examples
+#' \dontrun{
+#' get_inventors_df(pv_res)
+#' }
+#' 
+
+get_inventors_df <- function(pv_res){
+  
+  df_inventors <- pv_res$data$patents$inventors %>% 
+    stats::setNames(pv_res$data$patents$patent_number) %>% 
+    dplyr::bind_rows(.id = "patent_number") 
+  
+  return(df_inventors)
+  
+}
---
+++ R/get_patents_data.R
@@ -0,0 +1,30 @@
+#' Fetch USPTO patents data 
+#' 
+#' 
+#' @param query Takes in a query formated with `patentsview` domain specific language (DSL)
+#' @return List containing patents information
+#' 
+#' 
+#' @examples
+#' \dontrun{
+#' query <- with_qfuns( 
+#'  or(
+#'   text_all(patent_title = "artificial intelligence"),
+#'   text_all(patent_abstract = "artificial intelligence")
+#'   )
+#' )
+#' get_patents_data(query)
+#' }
+#' 
+
+get_patents_data <- function(query){
+  
+  fields <- patentsview::get_fields(
+    endpoint = c("patents"), 
+    groups = c("applications", "assignees", "cpcs", "patents", "examiners", "inventors", "lawyers", "wipos", "cited_patents", "citedby_patents") )
+  
+  pv_res <- patentsview::search_pv(query = query, fields = fields, all_pages = TRUE)
+  
+  return(pv_res)
+  
+}
