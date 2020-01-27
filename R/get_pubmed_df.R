751333d8dcad4cf7f9d72878b6a382b300d0dae4
Remy Gavard <r.gavard@gmail.com>
2020-01-27 15:39:05 +0000

    update RISmed utilities

--- R/get_pubmed_df.R
+++ R/get_pubmed_df.R
@@ -4,14 +4,13 @@
 #' @param query A pubmed query
 #' @return A tibble with scientific publication informations
 #' 
-#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
-#' get_examiners_df(pv_res)
+#' get_pubmed_df_general('query')
 #' }
 #' 
 #' @export
-get_pubmed_df <- function(query){
+get_pubmed_df_general <- function(query){
   
   search_query <- RISmed::EUtilsSummary(query, retmax=10000, mindate=1900,maxdate=2021)
   
@@ -23,10 +22,36 @@ get_pubmed_df <- function(query){
                             Year = RISmed::YearAccepted(records),
                             Authors = list(RISmed::Author(records)),
                             Journal = RISmed::Title(records),
-                            Mesh = RISmed::Mesh(records),
-                            PublicationType = RISmed::PublicationType(records),
                             Country = RISmed::Country(records),
                             Language = RISmed::Language(records))
+  
+  return(pubmed_data)
+  
+}
+
+
+#' Perform search on pudmed with RISmed and return a tibble with abstracts
+#' 
+#' 
+#' @param query A pubmed query
+#' @return A tibble with scientific publication informations
+#' 
+#' @examples
+#' \dontrun{
+#' get_pubmed_df_abstract('query')
+#' }
+#' 
+#' @export
+get_pubmed_df_abstract <- function(query){
+  
+  search_query <- RISmed::EUtilsSummary(query, retmax=10000, mindate=1900,maxdate=2021)
+  
+  records <- RISmed::EUtilsGet(search_query, type = "efetch", db = "pubmed")
+  
+  pubmed_data <- tibble::tibble(PMID = RISmed::PMID(records),    
+                                AbstractText = RISmed::AbstractText(records),
+                                Year = RISmed::YearAccepted(records),
+                                Country = RISmed::Country(records))
   
   return(pubmed_data)
   
